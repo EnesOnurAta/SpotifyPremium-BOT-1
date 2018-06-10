e21b2672586245cf67ada967c38ab055e857b9fc
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-10 17:10:43 +0300

    Update ekleyenler.js

--- commands/ekleyenler.js
+++ commands/ekleyenler.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 
 
 module.exports.run = async (bot, message, args) => {
-  
-  let msg =  bot.guilds.map(guild => `${message.guild.iconURL}**${guild.name}** Üye Sayısı: ${guild.memberCount}`).join('\n');
+  let sunucuiconu = message.guild.icon
+  let msg =  bot.guilds.map(guild => `${sunucuiconu} **${guild.name}** Üye Sayısı: ${guild.memberCount}`).join('\n');
   let embed = new Discord.RichEmbed()
   .setTitle(`Teşekkürler: ${bot.guilds.size} sunucu!`)
   .setDescription(`${msg}`)
