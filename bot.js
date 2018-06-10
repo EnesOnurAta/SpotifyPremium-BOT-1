e697a5858588978bd7077209657c29a138e5cfa7
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-10 17:34:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -223,25 +223,6 @@
         message.channel.send(List);
     }
 });
-	
-	//Yayın değiştirme
-	client.on = (client, message, args) => {
-  if (message.author.id !== ('274551537139712001')) return message.channel.send("Benim sahibim olduğun doğrulandı!");
-  const status = args.join(' ');
-  if (status.length === 0) {
-    const embed = new Discord.RichEmbed()
-      .setColor("#7289DA")
-      .setDescription(':negative_squared_cross_mark: Botun durumu değiştirildi!');
-    message.channel.send({ embed });
-  }
-
-  else if (status.length !== 0) {
-   client.user.setPresence({ game: { name: `${status}`, url: 'https://twitch.tv/enesonurata', type: 1 } });
-  const embed = new Discord.RichEmbed()
-    .setColor("#7289DA")
-    .setDescription(':white_check_mark: Yayını başarıyla değiştirdin');
-  message.channel.send({ embed });
-}};
 
 	//işte o devlet sırrı buraya yerleşecek :D
 	bot.login(process.env.BOT_TOKEN);
