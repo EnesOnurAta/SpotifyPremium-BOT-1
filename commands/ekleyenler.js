1d24313aedccc173457fe2ca49a34fb12c7c595a
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-10 17:32:07 +0300

    Update ekleyenler.js

--- commands/ekleyenler.js
+++ commands/ekleyenler.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
-
-
 module.exports.run = async (bot, message, args) => {
+  
   let msg =  bot.guilds.map(guild => `**${guild.name}** Üye Sayısı: ${guild.memberCount}`).join('\n');
   let embed = new Discord.RichEmbed()
   .setTitle(`Teşekkürler: ${bot.guilds.size} sunucu!`)
   .setDescription(`${msg}`)
-  .setColor("#ebf442")
+  .setColor("#ebf442");
   .setFooter("${bot.user.tag} botunu ekledğiniz için teşekkürler :heart:")
   .setTimestamp()
   message.channel.send(embed);
@@ -15,4 +14,3 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
   name: "ekleyenler"
 }
-
