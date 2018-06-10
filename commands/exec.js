61364d216fcec1b187515f9551712418a56701ae
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-10 14:56:23 +0300

    Update exec.js

--- commands/exec.js
+++ commands/exec.js
@@ -4,15 +4,15 @@ const Discord = require('discord.js');
 module.exports.run = async(bot, message, args, level) => {
     let embed = new Discord.RichEmbed()
   .setTitle("Evaluation")
-  .setDescription("Sorry, the `eval` command can only be executed by the Developer.")
+  .setDescription("Üzgünüm, `eval` komutunu sadece botun sahibi kullanabilir.")
   .setColor("#cdf785");
-  if(message.author.id !== '414111663076147201') return message.channel.send(embed);
+  if(message.author.id !== '274551537139712001') return message.channel.send(embed);
     exec(`${args.join(' ')}`, (error, stdout) => {
       const response = (error || stdout);
       let embed = new Discord.RichEmbed()
       .setTitle(`Executed in ${Math.round(bot.ping)}ms`)
-      .addField(":inbox_tray: Input", `\`\`\`asciidoc\n${args.join(" ")}\n\`\`\``)
-      .addField(":outbox_tray: Output", `\`\`\`js\n${response}\n\`\`\``)
+      .addField(":inbox_tray: Girdi", `\`\`\`asciidoc\n${args.join(" ")}\n\`\`\``)
+      .addField(":outbox_tray: Çıktı", `\`\`\`js\n${response}\n\`\`\``)
       .setColor('GREEN');
       message.channel.send({embed});
     });
